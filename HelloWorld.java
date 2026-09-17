5d09b85eb462fea23214750918715aee83444a69
Bara Al-Wabel <11bkvg11@gmail.com>
2026-09-17 08:46:29 +0300

    add gretingfeature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("hello github");
+        System.out.println("SE4111");
+
+
     }
 }
